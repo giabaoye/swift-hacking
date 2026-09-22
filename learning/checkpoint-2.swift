47c4f376a1838344166547f428d19bfda7890bc3
daboutt <baotran2115@gmail.com>
2026-09-22 23:13:57 +0700

    checkpoint 2

---
+++ learning/checkpoint-2.swift
@@ -0,0 +1,17 @@
+//
+//  checkpoint-2.swift
+//
+
+class CheckPointTwo {
+    let arr = ["a", "b", "d", "m", "a", "n", "m", "d", "o", "e", "m"]
+    
+    func execute() {
+        print("============")
+        print("Checkpoint 2")
+        print("number of items: \(arr.count)")
+        print("number of unique items: \(Set(arr).count)")
+        print("============")
+    }
+    
+}
+
