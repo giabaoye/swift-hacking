00e9843f619617d1efce52baca215f4d0a42efcb
daboutt <baotran2115@gmail.com>
2026-09-23 22:52:40 +0700

    checkpoint 3

---
+++ learning/checkpoint-3.swift
@@ -0,0 +1,20 @@
+//
+//  checkpoint-3.swift
+//  learning
+//
+
+class CheckPointThree {
+    func execute() {
+        for i in 1...100 {
+            if (i.isMultiple(of: 15)) {
+                print("FizzBuzz")
+            } else if (i.isMultiple(of: 3)) {
+                print("Fizz")
+            } else if (i.isMultiple(of: 5)) {
+                print("Buzz")
+            } else {
+                print(i)
+            }
+        }
+    }
+}
--- learning/main.swift
+++ learning/main.swift
@@ -2,10 +2,50 @@
 //  main.swift
 //  learning
 //
-//  Created by Gia Bao on 15/9/26.
-//
 
 import Foundation
 
-print("Hello, World!")
+do {
+    // Array
+    var arr = [Int]()
+    arr.append(0)
+    arr.append(4)
+    print("array: ", arr)
+
+    let characters = ["b", "a", "o"]
+    for character in characters {
+        print("This is \(character)")
+    }
+
+    // Dictionaries
+    var dictionary: [String: String] = [:]
+    dictionary["America"] = "New York"
+    dictionary["Vietnam"] = "Hanoi"
+    dictionary["Thailand"] = "Bangkok"
+
+    print("dictionary:", dictionary["Vietnam", default: "Unknown"], ",count:", dictionary.count)
+    dictionary.removeAll()
+    print("removed", dictionary)
+
+
+    // Sets
+    var cars = Set<String>([])
+    cars.insert("Ferrari")
+    cars.insert("Lamborghini")
+    print("set:",cars)
+
+    // ENUM
+    enum Weekday {
+        case monday, tuesday, wednesday, friday, saturday, sunday
+    }
+    var day: Weekday
+    day = .tuesday
+    day = .friday
+    print("Enum", day)
+    
+    let cp2 = CheckPointTwo()
+    cp2.execute()
+    let cp3 = CheckPointThree()
+    cp3.execute()
+}
 
